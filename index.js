c5c4d70914829a8aa0be388f575a9824837cea5a
Yurie <yurie.koga.a@gmail.com>
2020-01-10 14:05:30 -0800

    adding POST method

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const express = require("express");
-const app = express();
+const multer = require("multer");
+const uuidv4 = require("uuid/v4");
 
+const app = express();
+app.use(multer().none());
 app.use(express.static("web"));
 
-// app.get("/", (req, res) => res.send("Hello"));
+// ===== for fixed data =====
+// app.get("/api/v1/list", (req, res) => {
+//   const todoList = [
+//     { title: "todo-1", done: true },
+//     { title: "todo-2", done: false },
+//     { title: "todo-3", done: false }
+//   ];
 
+//   res.json(todoList);
+// });
+
+// ===== for flexible data =====
+const todoList = [];
 app.get("/api/v1/list", (req, res) => {
-  const todoList = [
-    { title: "todo-1", done: true },
-    { title: "todo-2", done: false },
-    { title: "todo-3", done: false }
-  ];
-
   res.json(todoList);
+});
+
+app.post("/api/v1/add", (req, res) => {
+  const todoData = req.body;
+  const todoTitle = todoData.title;
+  const id = uuidv4(); // create unique id
+  const todoItem = {
+    id,
+    title: todoTitle,
+    done: false
+  };
+
+  todoList.push(todoItem);
+  console.log("Add: " + JSON.stringify(todoItem));
+  res.json(todoItem);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
