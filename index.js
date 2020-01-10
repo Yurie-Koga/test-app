5a9a50546641d5b1405d881b14fdb86e6a1e3b32
Yurie <yurie.koga.a@gmail.com>
2020-01-10 13:00:39 -0800

    create index.html

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello"));
+app.use(express.static("web"));
+
+// app.get("/", (req, res) => res.send("Hello"));
+
+app.get("/api/v1/list", (req, res) => {
+  const todoList = [
+    { title: "todo-1", done: true },
+    { title: "todo-2", done: false },
+    { title: "todo-3", done: false }
+  ];
+
+  res.json(todoList);
+});
+
 app.listen(3000, () => console.log("Listening on port 3000"));
