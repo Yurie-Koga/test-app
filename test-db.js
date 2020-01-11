0d1633c0cc5979985bc159021345a84e48c089f1
Yurie <yurie.koga.a@gmail.com>
2020-01-11 13:24:33 -0800

    update test-db.js

--- test-db.js
+++ test-db.js
@@ -1,7 +1,6 @@
 const sqlite = require("sqlite3").verbose();
 
 // check access in-memory db
-<<<<<<< HEAD
 let db1 = new sqlite.Database(":memory:", err => {
   if (err) console.error("*** DB ERROR (db1): " + err.message);
   else console.log("+ Opened db1.");
@@ -9,15 +8,6 @@ let db1 = new sqlite.Database(":memory:", err => {
 db1.close(err => {
   if (err) console.error(err.message);
   else console.log("- Closed db1.");
-=======
-let db = new sqlite.Database(":memory:", err => {
-  if (err) console.error(err.message);
-  console.log("Connected to the in-memory SQlite database.");
-});
-db.close(err => {
-  if (err) console.error(err.message);
-  console.log("Closed db connection.");
->>>>>>> 63becf0a3e73db49fdb2b0a605572212af341934
 });
 
 // set open mode: default = sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE
@@ -25,7 +15,6 @@ let db2 = new sqlite.Database(
   "./db/example.db",
   sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE,
   err => {
-<<<<<<< HEAD
     if (err) console.error("*** DB ERROR (db2): " + err.message);
     else console.log("+ Opened db2.");
   }
@@ -33,15 +22,6 @@ let db2 = new sqlite.Database(
 db2.close(err => {
   if (err) console.error("*** DB ERROR (db2): " + err.message);
   else console.log("- Closed db2.");
-=======
-    if (err) console.error("DB ERROR: " + err.message);
-    console.log("Connected to example db.");
-  }
-);
-db2.close(err => {
-  if (err) console.error("DB ERROR: " + err.message);
-  console.log("Closed db2 connection.");
->>>>>>> 63becf0a3e73db49fdb2b0a605572212af341934
 });
 
 // set open mode (ReadWrite), see error
@@ -49,7 +29,6 @@ let db3 = new sqlite.Database(
   "./db/not-exist.db",
   sqlite.OPEN_READWRITE,
   err => {
-<<<<<<< HEAD
     if (err) console.error("*** DB ERROR (db3): " + err.message);
     else console.log("+ Opened db3.");
   }
@@ -84,13 +63,3 @@ db4.close(err => {
 });
 
 console.log();
-=======
-    if (err) console.error("DB ERROR: " + err.message);
-    console.log("Connected to not-exist.db.");
-  }
-);
-db3.close(err => {
-  if (err) console.error("DB ERROR: " + err.message);
-  console.log("Closed db3 connection.");
-});
->>>>>>> 63becf0a3e73db49fdb2b0a605572212af341934
