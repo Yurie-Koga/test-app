980f725623aec928238e2dbb353b94a16516c009
Yurie <yurie.koga.a@gmail.com>
2020-01-11 13:11:54 -0800

    update test-db.js

--- test-db.js
+++ test-db.js
@@ -1,13 +1,13 @@
 const sqlite = require("sqlite3").verbose();
 
 // check access in-memory db
-let db = new sqlite.Database(":memory:", err => {
-  if (err) console.error(err.message);
-  console.log("Connected to the in-memory SQlite database.");
+let db1 = new sqlite.Database(":memory:", err => {
+  if (err) console.error("*** DB ERROR (db1): " + err.message);
+  else console.log("+ Opened db1.");
 });
-db.close(err => {
+db1.close(err => {
   if (err) console.error(err.message);
-  console.log("Closed db connection.");
+  else console.log("- Closed db1.");
 });
 
 // set open mode: default = sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE
@@ -15,13 +15,13 @@ let db2 = new sqlite.Database(
   "./db/example.db",
   sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE,
   err => {
-    if (err) console.error("DB ERROR: " + err.message);
-    console.log("Connected to example db.");
+    if (err) console.error("*** DB ERROR (db2): " + err.message);
+    else console.log("+ Opened db2.");
   }
 );
 db2.close(err => {
-  if (err) console.error("DB ERROR: " + err.message);
-  console.log("Closed db2 connection.");
+  if (err) console.error("*** DB ERROR (db2): " + err.message);
+  else console.log("- Closed db2.");
 });
 
 // set open mode (ReadWrite), see error
@@ -29,11 +29,37 @@ let db3 = new sqlite.Database(
   "./db/not-exist.db",
   sqlite.OPEN_READWRITE,
   err => {
-    if (err) console.error("DB ERROR: " + err.message);
-    console.log("Connected to not-exist.db.");
+    if (err) console.error("*** DB ERROR (db3): " + err.message);
+    else console.log("+ Opened db3.");
   }
 );
 db3.close(err => {
-  if (err) console.error("DB ERROR: " + err.message);
-  console.log("Closed db3 connection.");
+  if (err) console.error("*** DB ERROR (db3): " + err.message);
+  else console.log("- Closed db3.");
 });
+
+// test serialize: control method calls order (Create -> Insert -> Select)
+let db4 = new sqlite.Database(":memory:", err => {
+  if (err) console.error("*** DB ERROR (db4): " + err.message);
+  else console.log("+ Opened db4.");
+});
+db4.serialize(() => {
+  db4
+    .run("CREATE TABLE greetings(message text)")
+    .run(
+      `INSERT INTO greetings(message)
+        VALUES('Hi'),
+              ('Hello'),
+              ('Hey')`
+    )
+    .each(`SELECT message FROM greetings`, (err, row) => {
+      if (err) throw err;
+      else console.log("Greetings: " + row.message);
+    });
+});
+db4.close(err => {
+  if (err) console.error("*** DB ERROR (db4): " + err.message);
+  else console.log("- Closed db4.");
+});
+
+console.log();
